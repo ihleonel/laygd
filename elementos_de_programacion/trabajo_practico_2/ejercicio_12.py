6ff36d2b162d96e6b6a355a86373168a203a32ec
ihleonel <ibarra.h.leonel@gmail.com>
2025-08-31 11:11:22 -0300

    Update: Mejorando ejercicio 12

--- elementos_de_programacion/trabajo_practico_2/ejercicio_12.py
+++ elementos_de_programacion/trabajo_practico_2/ejercicio_12.py
@@ -4,6 +4,8 @@ peso_muñeca_gr = 250
 cantidad_autos = int(input("Ingrese la cantidad de autos: "))
 cantidad_muñecas = int(input("Ingrese la cantidad de muñecas: "))
 
+cantidad_total = cantidad_autos + cantidad_muñecas
+
 peso_autos_total_gr = peso_auto_gr * cantidad_autos
 peso_muñecas_total_gr = peso_muñeca_gr * cantidad_muñecas
 
@@ -14,4 +16,4 @@ print("-" * 40)
 print(f"{'Auto':<9}", f"{cantidad_autos:>8}", f"{peso_autos_total_gr:>10}", f"{peso_autos_total_gr/1000:>10}")
 print(f"{'Muñeca':<9}", f"{cantidad_muñecas:>8}", f"{peso_muñecas_total_gr:>10}", f"{peso_muñecas_total_gr/1000:>10}")
 print("-" * 40)
-print(f"{'Totales':<9}", f"{cantidad_autos + cantidad_muñecas:>8}", f"{peso_total_gr:>10}", f"{peso_total_gr/1000:>10}")
+print(f"{'Totales':<9}", f"{cantidad_total:>8}", f"{peso_total_gr:>10}", f"{peso_total_gr/1000:>10}")
