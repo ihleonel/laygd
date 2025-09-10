bb7bacd6adb332636b0676bde66a03aed9be6fb3
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2025-09-10 19:50:13 -0300

    Update: trabajo practico 3

--- elementos_de_programacion/trabajo_practico_3/ejercicio_3.py
+++ elementos_de_programacion/trabajo_practico_3/ejercicio_3.py
@@ -4,7 +4,7 @@ num2 = int(input("Ingrese el segundo número entero: "))
 
 # Módulo (resto).
 modulo = num1 % num2
-print(f"El módulo (resto) de {num1} % {num2} es {modulo}")
+print(f"El módulo (resto) de {num1} y {num2} es {modulo}")
 
 # Potencia.
 potencia = num1 ** num2
@@ -12,4 +12,4 @@ print(f"La potencia de {num1} elevado {num2} es {potencia}")
 
 # División entera.
 division_entera = num1 // num2
-print(f"La división entera de {num1} // {num2} es {division_entera}")
+print(f"La división entera de {num1} y {num2} es {division_entera}")
