bd8041f02a9e942c5271f17784909c41e440316c
ihleonel <ibarra.h.leonel@gmail.com>
2025-09-09 21:40:49 -0300

    Update: Mejorando ejercicio 12

--- elementos_de_programacion/trabajo_practico_2/ejercicio_12.py
+++ elementos_de_programacion/trabajo_practico_2/ejercicio_12.py
@@ -17,7 +17,7 @@ peso_total_gr = peso_autos_total_gr + peso_muñecas_total_gr
 peso_total_kg = peso_total_gr/1000
 costo_total_kg = costo_autos_kg + costo_muñecas_kg
 
-print(f"{'Item':<10} {'Cantidad':>10} {'Peso gr.':>10} {'Peso kg.':>10} {'Costo $/kg':>16}")
+print(f"{'Item':<10} {'Cantidad':>10} {'Peso gr':>10} {'Peso kg':>10} {'Costo $/kg':>16}")
 print("-" * 60)
 print(f"{'Auto':<10} {cantidad_autos:>10} {peso_autos_total_gr:>10} {peso_autos_total_kg:>10} {costo_autos_kg:>16}")
 print(f"{'Muñeca':<10} {cantidad_muñecas:>10} {peso_muñecas_total_gr:>10} {peso_muñecas_total_kg:>10} {costo_muñecas_kg:>16}")
